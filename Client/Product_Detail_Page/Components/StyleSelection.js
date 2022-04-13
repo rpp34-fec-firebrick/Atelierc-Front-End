975d16ec535172d6df83763c9101982429a57672
Ahmad <a.almomani96@gmail.com>
2022-04-13 13:18:36 -0700

    Can log the correct Style when a style is clicked

--- Client/Product_Detail_Page/Components/StyleSelection.js
+++ Client/Product_Detail_Page/Components/StyleSelection.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import StyleRender from './SubComponentLevel1/StyleRender.js'
 
-function StyleSelection (props) {
+class StyleSelection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      styles : []
+    }
+    this.handleStyleClick = this.handleStyleClick.bind(this)
+  }
+  UNSAFE_componentWillReceiveProps (props) {
+    if (props !== undefined) {
+      this.setState({['styles']: props.styles})
+    }
+  }
+  handleStyleClick (event) {
+    console.log(event.target.name)
+  }
+
+  render() {
     return (
       <div>
-          {(props.styles.results) ?
-          props.styles.results.map((style) =>
-          <StyleRender style = {style} key = {style.style_id}/>)
+          {(this.state.styles.length !== 0) ?
+          this.state.styles.results?.map((style) =>
+            <StyleRender onclick={this.handleStyleClick.bind(this)}
+            style = {style} key = {style.style_id}
+            value = {style.style_id}/>)
           : null}
       </div>
     );
+  }
 }
+
 
 export default StyleSelection;
--- Client/Product_Detail_Page/Components/SubComponentLevel1/StyleRender.js
+++ Client/Product_Detail_Page/Components/SubComponentLevel1/StyleRender.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 function StyleRender (props) {
     return (
-      <div>
-          <img className ='styleRender' src = {props.style.photos[0].url}/>
-      </div>
+      <button onClick={props.onclick}>
+          <img className ='styleRender'
+          src = {props.style.photos[0].url} name ={props.value}/>
+      </button>
     );
 }
 
