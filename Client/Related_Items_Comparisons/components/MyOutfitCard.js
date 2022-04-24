fd40bfbc7f2aa5dabe169a2607a480ca323a6b5a
chowkingman <>
2022-04-23 23:42:08 -0500

    complete comparison modal and table.

--- Client/Related_Items_Comparisons/components/MyOutfitCard.js
+++ Client/Related_Items_Comparisons/components/MyOutfitCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class MyOutfit extends React.Component {
+class MyOutfitCard extends React.Component {
   constructor(props) {
     super(props);
 
@@ -18,4 +18,4 @@ class MyOutfit extends React.Component {
   }
 }
 
-export default MyOutfit;
+export default MyOutfitCard;
