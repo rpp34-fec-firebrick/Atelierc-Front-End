f1849fba54bbd1761af4d48ce4d95d4923329cfb
chowkingman <>
2022-04-29 17:17:11 -0500

    completed 95% of the related comparison functions and 100% of the CSS

--- Client/Related_Items_Comparisons/components/MyOutfitCard.js
+++ Client/Related_Items_Comparisons/components/MyOutfitCard.js
@@ -6,38 +6,34 @@ class MyOutfitCard extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-    };
+    this.state = {}
+
     this.onCardSelect = this.onCardSelect.bind(this);
-    this.onStarSelect = this.onStarSelect.bind(this);
   }
 
   onCardSelect = () => {
     this.props.eventHandler(Number(this.props.product.id));
   };
 
-  onStarSelect = () => {
-    //a modal will show
-    //all product details will be listed inside that modal
-    //when click outside the modal
-    //the modal will disappear
-  };
-
   render() {
     return (
       <div className="myproduct list-card">
-        <img className="media-object" src={this.props.product.thumbnail_url} alt="your outfit product image" />
+        <img
+          className="media-object"
+          src={this.props.product.thumbnail_url}
+          alt="your outfit product image" />
         <svg
           className="cross symbol"
           width="24"
           height="24"
           xmlns="http://www.w3.org/2000/svg"
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          onClick={this.props.onClickMyOutfitDeleteEvent(this.props.product)}>
+          onClick={() => {
+            this.props.handleDeleteClick(this.props.product.id);
+            this.props.onClickMyOutfitDeleteEvent(this.props.product.id);
+          }}>
           <path d="M12 11.293l10.293-10.293.707.707-10.293 10.293 10.293 10.293-.707.707-10.293-10.293-10.293 10.293-.707-.707 10.293-10.293-10.293-10.293.707-.707 10.293 10.293z" />
         </svg>
-        <div classname="card-info">
+        <div className="card-info">
           <p className="product-list-card-cat">{this.props.product.category}</p>
           <p className="product-list-card-name" onClick={this.onCardSelect}>{this.props.product.name}</p>
           <p className="product-list-card-price">${this.props.product.default_price}</p>
@@ -46,5 +42,6 @@ class MyOutfitCard extends React.Component {
     )
   }
 };
+// this.props.onClickMyOutfitDeleteEventHandler(this.props.product);
 
 export default MyOutfitCard;
--- Client/Related_Items_Comparisons/components/RelatedCard.js
+++ Client/Related_Items_Comparisons/components/RelatedCard.js
@@ -114,7 +114,7 @@ class RelatedCard extends React.Component {
           onClick={this.onStarSelect}>
           <path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z" />
         </svg>
-        <div classname="card-info">
+        <div className="card-info">
           <Modal show={this.state.show} handleClose={this.hideModal} prodCharaData={this.state.prodCharaData}></Modal>
           <p className="product-list-card-cat">{this.props.product.category}</p>
           <p className="product-list-card-name" onClick={this.onCardSelect}>{this.props.product.name}</p>
