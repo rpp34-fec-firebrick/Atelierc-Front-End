961cc7c5e2e7e37034cea3baee78cfd8b26558bf
Keegan Wolf <kwolf313@live.com>
2022-04-29 21:26:04 -0500

    Changed AWS S3 variables from Auth.js to .env

--- Client/Questions_Answers/s3-helpers.js
+++ Client/Questions_Answers/s3-helpers.js
@@ -2,10 +2,10 @@ const AUTH = require('../../Auth.js');
 const fs = require('fs');
 const S3 = require('aws-sdk/clients/s3');
 
-const bucketName = AUTH.AWS_BUCKET_NAME;
-const region = AUTH.AWS_BUCKET_REGION;
-const accessKeyId = AUTH.AWS_ACCESS_KEY;
-const secretAccessKey = AUTH.AWS_SECRET_KEY;
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
 
 const s3 = new S3 ({
   region: region,
